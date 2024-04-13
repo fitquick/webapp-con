b4de012882f085345f98b4332551db005df832f0
fitquick <141992941+fitquick@users.noreply.github.com>
2024-04-13 17:18:22 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -7,7 +7,7 @@ export const APP_INFO: AppInfo = {
   description: '',
   copyright: '',
   privacy_policy: '',
-  default_language: 'en-US',
+  default_language: 'ja',
 }
 export const isChatStarted = true
 export const isShowPrompt = false
